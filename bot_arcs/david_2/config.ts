bf974bef428e6e1c2a639488aaa6164b07027367
max bittker <maxbittker@gmail.com>
2026-01-27 21:52:30 -0500

    remove long arcs

--- bot_arcs/david_2/config.ts
+++
@@ -1,4 +0,0 @@
-export const character = {
-  username: 'david_2',
-  goal: 'Explore the game, enjoy yourself, and make your own goals'
-};
--- bot_arcs/david_3/config.ts
+++
@@ -1,4 +0,0 @@
-export const character = {
-  username: 'david_3',
-  goal: 'Explore the game, enjoy yourself, and make your own goals'
-};
